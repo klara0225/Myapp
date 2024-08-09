c2a524d89c598ee7b0643030b3ad1480c2caba6a
Kasumy <kasumy.lara1@gmail.com>
2024-08-08 19:07:24 -0700

    localStoragepractice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EmployeeForm from './Component/Employeeform';
 
@@ -9,13 +9,18 @@ function App() {
   const addEmployee = (employee) => {
     setEmployees([...employees, employee]);
   };
+  const saveData = () =>{
+    localStorage.setItem('employees', JSON.stringify(employees));
 
+  };
   return (
+    
   <Router>
     <div className="App">
         <Routes>
             <Route path = "/" element={<EmployeeForm addEmployee={addEmployee}/>}/>
         </Routes>
+        <button onClick={saveData}>Save Data</button>
     </div>
   </Router>
   );
